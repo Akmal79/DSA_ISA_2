f3b9b08deb1581496919a9668ea07c0e1708a17b
Akmal79 <109073778+Akmal79@users.noreply.github.com>
2024-02-19 22:58:05 +0530

    Update Right_threaded_tree.c

--- Right_threaded_tree.c
+++ Right_threaded_tree.c
@@ -1,79 +1,71 @@
 #include <stdio.h>
 #include <stdlib.h>
-struct node
-{
+
+struct node {
     int data;
     struct node *left;
     struct node *right;
-    int rthread; //***
-} *root = NULL;
-void main()
-{
-    int choice, item;
-    do
-    {
-        printf("\nEnter the item which you want to insert?\n");
-        scanf("%d", &item);
-        insert(item);
-        printf("\nPress 1 to insert nodes more nodes in tree \n");
-        scanf("%d", &choice);
-    } while (choice == 1);
+    int rthread;
+};
+
+struct node *root = NULL;
+
+void insert(int item) {
+    // Allocate memory for new node
+    struct node *ptr = (struct node *)malloc(sizeof(struct node));
+    if (ptr == NULL) {
+        printf("Memory allocation failed.\n");
+        return;
+    }
+
+    // Initialize node data and threads
+    ptr->data = item;
+    ptr->left = NULL;
+    ptr->rthread = 1; // Right child initially a thread
+
+    if (root == NULL) {
+        root = ptr;
+    } else {
+        // Find insertion position and update thread pointers
+        struct node *parentptr = NULL;
+        struct node *nodeptr = root;
+        while (nodeptr != NULL) {
+            parentptr = nodeptr;
+            if (item < nodeptr->data) {
+                nodeptr = nodeptr->left;
+            } else {
+                // Update thread if no right child
+                if (nodeptr->rthread == 0) {
+                    nodeptr->right = ptr;
+                } else {
+                    break; // Reached in-order successor's thread
+                }
+            }
+        }
+
+        // Link new node based on position
+        if (item < parentptr->data) {
+            parentptr->left = ptr;
+            ptr->right = parentptr; // Right child points to parent
+        } else {
+            ptr->right = parentptr->right;
+            parentptr->right = ptr;
+            parentptr->rthread = 0; // Update parent's right thread
+        }
+    }
 }
-void insert(int item)
-{
-    struct node *ptr, *parentptr, *nodeptr;
 
-    ptr = (struct node *)malloc(sizeof(struct node));
-    if (ptr == NULL)
-    {
-        printf("can't insert");
+void inorder_traversal() {
+    struct node *ptr = root;
+    while (ptr != NULL) {
+        // Follow right child pointers or threads
+        printf("%d ", ptr->data);
+        ptr = (ptr->rthread == 1) ? NULL : ptr->right;
     }
-    else
-    {
-        ptr->data = item;
-        ptr->left = NULL;
-        ptr->right = NULL;
-        ptr->rthread = 1;//***
-        if (root == NULL)
-        {
-            root = ptr;
-        }
-        else
-        {
-            parentptr = NULL;
-            nodeptr = root;
-            while (nodeptr != NULL)
-            {
-                parentptr = nodeptr;
-                if (item < nodeptr->data)
-                {
-                    nodeptr = nodeptr->left;
-                }
-                else//***
+    printf("\n");
+}
 
-                {
-                    if (nodeptr->rthread == 0)
-                    {
-                        nodeptr = nodeptr->right;
-                    }
-                    else
-                    {
-                        break;
-                    }
-                }
-            }
-            if (item < parentptr->data)
-            {
-                parentptr->left = ptr;
-                ptr->right = parentptr;//***
-            }
-            else
-            {
-                ptr->right = parentptr->right;  //***
-                parentptr->right = ptr;
-                parentptr->rthread = 0;  //***
-            }
-        }
-        printf("Node Inserted");
-    }
+int main() {
+    // Insert nodes and perform in-order traversal
+    return 0;
 }
