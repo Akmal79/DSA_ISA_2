0f8eb12e52b3b6f7c050f8346b68dfb58e57dd39
Akmal79 <109073778+Akmal79@users.noreply.github.com>
2024-02-19 22:57:41 +0530

    Update Right_threaded_tree.c

--- Right_threaded_tree.c
+++ Right_threaded_tree.c
@@ -1,31 +1,79 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+struct node
+{
+    int data;
+    struct node *left;
+    struct node *right;
+    int rthread; //***
+} *root = NULL;
 void main()
 {
-inorder();
+    int choice, item;
+    do
+    {
+        printf("\nEnter the item which you want to insert?\n");
+        scanf("%d", &item);
+        insert(item);
+        printf("\nPress 1 to insert nodes more nodes in tree \n");
+        scanf("%d", &choice);
+    } while (choice == 1);
 }
-void inorder()
+void insert(int item)
 {
-struct node *p=root;
-struct node *q;
-do
-{
-q=NULL;
-while(p!=NULL)
-{
-q=p;
-p=p->left;
-}
-if(q!=NULL)
-{
-printf("\t %d",q->data);
-p=q->right;
-while(q->rthread&&p!=NULL)
-{
+    struct node *ptr, *parentptr, *nodeptr;
 
-printf("\t %d",p->data);
-q=p;
-p=p->right;
-}
-}
-}while(q!=NULL);
+    ptr = (struct node *)malloc(sizeof(struct node));
+    if (ptr == NULL)
+    {
+        printf("can't insert");
+    }
+    else
+    {
+        ptr->data = item;
+        ptr->left = NULL;
+        ptr->right = NULL;
+        ptr->rthread = 1;//***
+        if (root == NULL)
+        {
+            root = ptr;
+        }
+        else
+        {
+            parentptr = NULL;
+            nodeptr = root;
+            while (nodeptr != NULL)
+            {
+                parentptr = nodeptr;
+                if (item < nodeptr->data)
+                {
+                    nodeptr = nodeptr->left;
+                }
+                else//***
+
+                {
+                    if (nodeptr->rthread == 0)
+                    {
+                        nodeptr = nodeptr->right;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+            if (item < parentptr->data)
+            {
+                parentptr->left = ptr;
+                ptr->right = parentptr;//***
+            }
+            else
+            {
+                ptr->right = parentptr->right;  //***
+                parentptr->right = ptr;
+                parentptr->rthread = 0;  //***
+            }
+        }
+        printf("Node Inserted");
+    }
 }
